17eb6371b734d1bc7285ed308d85128022dbc78a
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-08 17:38:13 +0000

    iki tane test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class test {
     @Test
-    public void test1(){
-int a = 2;
-int b = 5;
+    public void test1() {
+        int a = 2;
+        int b = 5;
         Assert.assertEquals(7, a + b);
+    }
+
+    @Test
+    public void test2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.w3schools.com/java/java_modifiers.asp");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("w3schools"));
     }
 }
