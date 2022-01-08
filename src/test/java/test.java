7bbd71f4a9f22d3e0facb45bd4c609072bd4d605
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-08 21:45:32 +0000

    browser eklendi

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,14 +16,26 @@ public class test {
         Assert.assertEquals(3, b-a,"cikarma isleminde hata var mi");
 
     }
+    ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
 
     @Test(groups = "smoke")
     public void test2() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.w3schools.com/java/java_modifiers.asp");
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("w3schools"),"websitesinde hata var");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions optionsAWS = new ChromeOptions();
+        optionsAWS.addArguments("disable-gpu");
+        optionsAWS.setHeadless(true);
+        optionsAWS.addArguments("--no-sandbox");
+        optionsAWS.addArguments("headless");
+        optionsAWS.addArguments("--disable-dev-shm-usage");
+        optionsAWS.addArguments("--disable-extensions");
+        optionsAWS.addArguments("--allow-insecure-localhost");
+        optionsAWS.addArguments("--single-process");
+        webDriver.set(new ChromeDriver(optionsAWS));
+
+        webDriver.get().get("https://www.w3schools.com/java/java_modifiers.asp");
+
+        Assert.assertTrue(webDriver.get().getCurrentUrl().contains("w3schools"),"websitesinde hata var");
 
     }
 }
