708705571db5e3fab6f1dff74cff6b0ad6d7edde
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-08 17:30:12 +0000

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test1(){
+int a = 2;
+int b = 5;
+        Assert.assertEquals(7, a + b);
+    }
+}
