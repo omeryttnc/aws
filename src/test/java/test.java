b17d8deb8fafb2a4c24069222c30d1adf6b4b2c2
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-08 18:17:09 +0000

    iki tane test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,25 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class test {
-    @Test
+    @Test(groups = "smoke")
     public void test1() {
         int a = 2;
         int b = 5;
-        Assert.assertEquals(11, a + b);
+        Assert.assertEquals(11, a + b,"toplama isleminde hata var");
     }
 
-    @Test
+    @Test(groups = "smoke")
     public void test2() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.w3schools.com/java/java_modifiers.asp");
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("w3kkkschools"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("w3kkkschools"),"websitesinde hata var");
 
     }
 }
