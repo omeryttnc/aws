171e537a8afc0f740d2a6b8475f4facfdb46aebc
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-08 17:45:23 +0000

    iki tane test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -10,7 +10,7 @@ public class test {
     public void test1() {
         int a = 2;
         int b = 5;
-        Assert.assertEquals(7, a + b);
+        Assert.assertEquals(8, a + b);
     }
 
     @Test
